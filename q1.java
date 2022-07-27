5ae51d1ac3f10e25900814310122d5d9cd2d7688
chenfeng198510 <103224705+chenfeng198510@users.noreply.github.com>
2022-07-26 22:09:34 -0700

    q1 and q2

---
+++ q1.java
@@ -0,0 +1,33 @@
+package assignment3;
+
+import java.util.Scanner;
+     interface AdvancedArithmetic {
+        int divisor_sum(int n);
+    }
+
+
+    public class q1
+    {
+        public static void main(String[] args) {
+            System.out.println("Enter n: ");
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            MyCalculator c = new MyCalculator();
+            System.out.println(c.divisor_sum(n) + "\n");
+        }
+    }
+
+
+    class MyCalculator implements AdvancedArithmetic{
+
+        public int divisor_sum(int n){
+            int sum1 = 1;
+            for (int i=2;i<=n;i++){
+                if (n%i == 0){
+                    sum1 = sum1 + i;
+                }
+            }
+            return sum1;
+        }
+    }
+
